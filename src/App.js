55bd7aa7970161aaf2eb70003d659cbd4c98bafe
alvina-dr <alvinadr24@gmail.com>
2021-11-24 10:12:04 +0100

    ajout base memory pokemon

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Pokemon memory</h1>
+            <button>Start New Game</button>
+        </div>
+    );
+}
+
+export default App
