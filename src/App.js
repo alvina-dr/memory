18f529d888025843270225f89c076ccb44575ad0
Astialas <cyrilbas40@gmail.com>
2021-11-24 10:48:40 +0100

    card grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import { useState } from 'react'
 import './App.css'
 
+const cardImages = [
+    { "src": "/img/test.png" },
+    { "src": "/img/test1.png" },
+    { "src": "/img/test2.png" },
+    { "src": "/img/test3.png" },
+    { "src": "/img/test4.png" },
+    { "src": "/img/test5.png" },
+]
+
 function App() {
+    const [cards, setCards] = useState([])
+    const [turns, setTurns] = useState(0)
+
+    //shuffle cards
+    const shuffleCards = () => {
+        const shuffledCards = [...cardImages, ...cardImages]
+            .sort(() => Math.random() - 0.5)
+            .map((card) => ({ ...card, id: Math.random() }))
+
+        setCards(shuffledCards)
+        setTurns(0)
+    }
+
+    console.log(cards, turns)
+
     return (
         <div className="App">
             <h1>Pokemon memory</h1>
-            <button>Start New Game</button>
+            <button onClick={shuffleCards}>Start New Game</button>
+
+            <div className="card-grid">
+                {cards.map(card => (
+                    <div className="card" key={card.id}>
+                        <div>
+                            <img className="front" src={card.src} alt="card front" />
+                            <img className="back" src="/img/cover.png" alt="card back" />
+                        </div> 
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
