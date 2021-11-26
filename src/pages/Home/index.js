34434b8fa86f8445572c0b2b5ffe2912303ee844
alvina-dr <alvinadr24@gmail.com>
2021-11-26 17:28:38 +0100

    ajout phrase dans home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,9 @@ function Home() {
     return (
         <div className="Home">
             <Header/>
-            <p>Welcome to our website.<br/>Here you can play a pokemon memory game.</p>
+            <p>Welcome to our website.</p>
+            <p>We are Cyril BAS and Alvina DAMASIO--RAZAFIARIZAKA.</p> 
+            <p><br/>Here you can play a pokemon memory game.</p>
             <Button link="/memory">
                 Pokemon Memory       
             </Button>
