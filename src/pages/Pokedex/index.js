93ad790d54531c18a9d0626c8e100552095c4f51
alvina-dr <alvinadr24@gmail.com>
2021-11-26 09:22:07 +0100

    modif card pour build netlify

--- src/pages/Pokedex/index.js
+++ src/pages/Pokedex/index.js
@@ -1,45 +1,45 @@
 import * as React from 'react';
-import { useState } from 'react/cjs/react.development';
+// import { useState } from 'react/cjs/react.development';
 import Header from '../../components/Header';
 import PokedexCard from '../../components/PokedexCard';
 import './style.css';
 
-const cardImages = [
-    { "src": "/img/1.png"},
-    { "src": "/img/2.png"},
-    { "src": "/img/3.png"},
-    { "src": "/img/4.png"},
-    { "src": "/img/5.png"},
-    { "src": "/img/6.png"},
-    { "src": "/img/7.png"},
-    { "src": "/img/8.png"},
-    { "src": "/img/9.png"},
-    { "src": "/img/10.png"},
-    { "src": "/img/11.png"},
-    { "src": "/img/12.png"},
-    { "src": "/img/13.png"},
-    { "src": "/img/14.png"},
-    { "src": "/img/15.png"},
-    { "src": "/img/16.png"},
-    { "src": "/img/17.png"},
-    { "src": "/img/18.png"},
-    { "src": "/img/19.png"},
-    { "src": "/img/20.png"}
-]
+// const cardImages = [
+//     { "src": "/img/1.png"},
+//     { "src": "/img/2.png"},
+//     { "src": "/img/3.png"},
+//     { "src": "/img/4.png"},
+//     { "src": "/img/5.png"},
+//     { "src": "/img/6.png"},
+//     { "src": "/img/7.png"},
+//     { "src": "/img/8.png"},
+//     { "src": "/img/9.png"},
+//     { "src": "/img/10.png"},
+//     { "src": "/img/11.png"},
+//     { "src": "/img/12.png"},
+//     { "src": "/img/13.png"},
+//     { "src": "/img/14.png"},
+//     { "src": "/img/15.png"},
+//     { "src": "/img/16.png"},
+//     { "src": "/img/17.png"},
+//     { "src": "/img/18.png"},
+//     { "src": "/img/19.png"},
+//     { "src": "/img/20.png"}
+// ]
 
 function Pokedex() {
 
-    const [cards, setCards] = useState([]);
+    // const [cards, setCards] = useState([]);
 
-    const shuffledCards = [...cardImages, ...cardImages]
-    .sort(() => Math.random() - 0.5)
-    .map((card) => ({ ...card, id: Math.random() }))
+    // const shuffledCards = [...cardImages, ...cardImages]
+    // .sort(() => 1, -1)
+    // .map((card) => ({ ...card, id: Math.random() }))
 
 
-    //LANCE LE JEU + REMET TIMER À ZÉRO + MÉLANGE LES CARTES
-    const shuffleCards = () => {
-        setCards(shuffledCards)
-    }
+    // //LANCE LE JEU + REMET TIMER À ZÉRO + MÉLANGE LES CARTES
+    // const shuffleCards = () => {
+    //     setCards(shuffledCards)
+    // }
 
     return (
         <div className="Pokedex">
@@ -48,7 +48,8 @@ function Pokedex() {
             <div className="card-grid">
             {cards.map(card => (
                 <PokedexCard
-                    card={card}/>
+                key={card.id}
+                card={card}/>
                 ))}
             </div>
         </div>
