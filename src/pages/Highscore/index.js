34a66e8290593a09bc06a0e710d1f402ff7d0c8d
alvina-dr <alvinadr24@gmail.com>
2021-11-26 17:45:57 +0100

    ajout du choix du pseudo dans highscore

--- src/pages/Highscore/index.js
+++ src/pages/Highscore/index.js
@@ -1,14 +1,28 @@
 import * as React from 'react';
 import Header from '../../components/Header';
 import './style.css';
+import { useState, useEffect } from "react";
 
 
 function Highscore() {
  
+    const [pseudo, setPseudo] = useState(() => {
+        // getting stored value
+        const saved = localStorage.getItem("pseudo");
+        const initialValue = JSON.parse(saved);
+        return initialValue || "";
+      });
+
+    useEffect(() => {
+        localStorage.setItem("pseudo", JSON.stringify(pseudo));
+    }, [pseudo]);
+
     return (
         <div className="Highscore">
             <Header/>
             <p>Highscore</p>
+            <label>Choose your pseudo :</label><br/>
+            <input type="text" value={pseudo} onChange={(e) => {setPseudo(e.target.value)}} placeholder="Pseudo" ></input>
         </div>
 
 
